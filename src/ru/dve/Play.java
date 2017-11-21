53d0f445d99699bd976a3056ac794b5a96bd08fd
dubin <deznod>
2017-11-21 23:46:20 +0300

    Рефакторинг классов

--- src/ru/dve/Play.java
+++ src/ru/dve/Play.java
@@ -3,7 +3,7 @@ package ru.dve;
 /**
  * Created by dubin on 30.10.2017.
  */
-public class Play {
+public class Play  {
     public static void main(String[] args){
         Window window = new Window();
     }
